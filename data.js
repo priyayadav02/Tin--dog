a27e64799ebf1b319ead13a22c0a3156fa0fede3
priyayadav02 <129416984+priyayadav02@users.noreply.github.com>
2024-01-01 17:29:55 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,36 @@
+const dogsData = [
+  {
+    name: "Bella",
+    avatar: "images/dog-bella.jpg",
+    age: 43,
+    bio: "Yup, that's my owner. U can meet him if you want",
+    hasBeenSwiped: false,
+    hasBeenLiked: false,
+  },
+  {
+    name: "Rex",
+    avatar: "images/dog-rex.jpg",
+    age: 25,
+    bio: "Art. Literature. Natural wine. Yoga.",
+    hasBeenSwiped: false,
+    hasBeenLiked: false,
+  },
+  {
+    name: "Teddy",
+    avatar: "images/dog-teddy.jpg",
+    age: 30,
+    bio: "How you doin?",
+    hasBeenSwiped: false,
+    hasBeenLiked: false,
+  },
+  {
+    name: "Oscar",
+    avatar: "images/dog-oscar.png",
+    age: 32,
+    bio: "All up to date on current affairs.",
+    hasBeenSwiped: false,
+    hasBeenLiked: false,
+  },
+];
+
+export default dogsData;
